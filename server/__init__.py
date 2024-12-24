bcf28fe6e2dfc9061237931f7d33871a7914899b
Ibrahem-Ali99 <s-ibrahem.ali@zewailcity.edu.eg>
2024-12-25 00:06:11 +0200

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
+#none
 from flask import Flask
 from flask_cors import CORS
 from flask_mail import Mail
